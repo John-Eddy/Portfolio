1d23e26fa1379d6718cf3f183706fe340b53b2e0
rahmanieddy@gmail.com <rahamnieddy@gmail.com>
2016-04-04 17:48:11 +0200

    04/04/2016

---
+++ js/test.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Eddy on 04/04/2016.
+ */
+
+/**
+ * Created by Utilisateur on 29/03/2016.
+ */
+$('#webdesign').bind('inview', function(event) {
+
+    setTimeout(animationWebdesign,0);
+
+});
+
+function animationWebdesign() {
+
+        $('.webdesign').easyPieChart({
+            scaleColor: "#ecf0f1",
+            lineWidth: 20,
+            lineCap: 'butt',
+            barColor: '#1abc9c',
+            trackColor:	"#ecf0f1",
+            size: 160,
+            animate: 3000,
+        });
+
+
+}
+
+$('#devweb').bind('inview', function(event) {
+
+    setTimeout(animationDevWeb,0);
+
+});
+
+function animationDevWeb() {
+
+    $('.devweb').easyPieChart({
+        scaleColor: "#ecf0f1",
+        lineWidth: 20,
+        lineCap: 'butt',
+        barColor: '#1abc9c',
+        trackColor:	"#ecf0f1",
+        size: 160,
+        animate: 3000,
+    });
+
+
+}
